06625ebea91091a375845ee6f32a927a0e6f4fcc
Joon. J <jtj308@naver.com>
2019-09-13 17:57:18 +0900

    First commit

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/joon/Documents/Dev/python/udemy_django_dev/btre_project/venv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
